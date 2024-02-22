750c93e842e2afaf907171e3735b315ac8e6edef
Douglas Martinez <douggmartinez12@gmail.com>
2024-02-21 23:58:26 -0600

    adding functional nav-bar

--- zelaya-roofing/src/components/Navbar.js
+++ zelaya-roofing/src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <div className='header'>
       <Link to='/'><h1>Zelaya Roofing</h1></Link>
-      <ul className='nav-menu'>
+      <ul className={click ? 'nav-menu active' : 'nav-menu'}>
         <li>
           <Link to='/'>Home</Link>
         </li>
