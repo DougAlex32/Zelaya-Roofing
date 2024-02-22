a4e1a48f304ebf65e9c38962a8566ff9031f7bb0
Douglas Martinez <douggmartinez12@gmail.com>
2024-02-22 17:28:33 -0600

    adding to video.js and changinf video.css

--- zelaya-roofing/src/components/Video.js
+++ zelaya-roofing/src/components/Video.js
@@ -1,25 +1,29 @@
-import React from 'react'
-import './VideoStyles.css'
-import { Link } from 'react-router-dom'
-import roofingVideo from '../assets/roofing.mp4'
-
+import React from "react";
+import "./VideoStyles.css";
+import { Link } from "react-router-dom";
+import roofingVideo from "../assets/roofing.mp4";
 
 const Video = () => {
   return (
-    <div className='hero'>
-        <video autoPlay loop muted id='video'>
-            <source src={roofingVideo} type="video/mp4" />
-        </video>
-    <div className='content'>
+    <div className="hero">
+      <video autoPlay loop muted id="video">
+        <source src={roofingVideo} type="video/mp4" />
+      </video>
+      <div className="content">
         <h1>Zelaya Roofing</h1>
         <p>Building Homes with A Foundation In God.</p>
-    </div>
-      <div>
-        <Link to='/about' className='btn'>Learn More</Link>
-        <Link to='/contact' className='btn btn-light'>Let's Talk</Link>
+
+        <div>
+          <Link to="/about" className="btn">
+            Let's Talk
+          </Link>
+          <Link to="/contact" className="btn btn-light">
+            Learn More
+          </Link>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Video
+export default Video;
